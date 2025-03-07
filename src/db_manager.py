aa51a54467e5040094e2a23f87d4ae81c402bf29
Mariusz Huflejt <huflejtm@gmail.com>
2025-03-07 12:10:01 +0100

    Add singleton in DatabaseManager

--- src/db_manager.py
+++ src/db_manager.py
@@ -1,40 +1,45 @@
 from sqlalchemy import (Table, Column, Integer, Float, String, ForeignKey, UniqueConstraint, MetaData,create_engine)
 
-metadata = MetaData()
+_metadata = MetaData()
 
 # tables
-coins = Table('coin', metadata,
-                Column('id', String(50), primary_key=True),
-                Column('symbol', String(10), unique=True),
-                Column('name', String(50), unique=True))
+coins = Table('coin', _metadata,
+              Column('id', String(50), primary_key=True),
+              Column('symbol', String(10), unique=True),
+              Column('name', String(50), unique=True))
 
-currencies = Table('currency', metadata,
-                Column('symbol', String(5), primary_key=True))
+currencies = Table('currency', _metadata,
+                   Column('symbol', String(5), primary_key=True))
 
-timestamps = Table('timestamp', metadata,
-                Column('id', Integer, primary_key=True, autoincrement=True),
-                Column('coin_id', String(50), ForeignKey('coin.id')),
-                Column('currency_symbol', String(5), ForeignKey('currency.symbol')),
-                Column('timestamp', Integer),
-                UniqueConstraint('coin_id', 'currency_symbol', 'timestamp', name='uq_timestamp_entry'))
+timestamps = Table('timestamp', _metadata,
+                   Column('id', Integer, primary_key=True, autoincrement=True),
+                   Column('coin_id', String(50), ForeignKey('coin.id')),
+                   Column('currency_symbol', String(5), ForeignKey('currency.symbol')),
+                   Column('timestamp', Integer),
+                   UniqueConstraint('coin_id', 'currency_symbol', 'timestamp', name='uq_timestamp_entry'))
 
-ohlc_data = Table('ohlc_data', metadata,
-                Column('id', Integer,ForeignKey('timestamp.id'), primary_key=True),
-                Column('open', Float),
-                Column('high', Float),
-                Column('low', Float),
-                Column('close', Float))
+ohlc_data = Table('ohlc_data', _metadata,
+                  Column('id', Integer,ForeignKey('timestamp.id'), primary_key=True),
+                  Column('open', Float),
+                  Column('high', Float),
+                  Column('low', Float),
+                  Column('close', Float))
 
-historical_data = Table('historical_data', metadata,
-                Column('id', Integer, ForeignKey('timestamp.id'), primary_key=True),
-                Column('total_volume', Integer),
-                Column('market_cap', Integer),
-                Column('price', Float))
+historical_data = Table('historical_data', _metadata,
+                        Column('id', Integer, ForeignKey('timestamp.id'), primary_key=True),
+                        Column('total_volume', Integer),
+                        Column('market_cap', Integer),
+                        Column('price', Float))
 
 class DatabaseManager:
-    def __init__(self, db_url="sqlite:///crypto.db"):
-        self.engine = create_engine(db_url)
-        metadata.create_all(self.engine)
+    _instance = None
+
+    def __new__(cls, db_url="sqlite:///crypto.db"):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.engine = create_engine(db_url)
+            _metadata.create_all(cls._instance.engine)
+        return cls._instance
 
     def get_engine(self):
         return self.engine
