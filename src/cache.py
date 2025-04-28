4e5723ee0cf925652edbf35d9a01990f57b035f0
Mariusz Huflejt <huflejtm@gmail.com>
2025-04-28 08:38:28 +0200

    Refactor cache

--- src/cache.py
+++ src/cache.py
@@ -11,12 +11,17 @@ from db_schema import metadata, timestamps, staging
 from frames import make_time_series_frame
 
 
+def _n_min_ago_timestamp(n: int) -> float:
+    """Returns the timestamp of the moment that was n minutes ago in the format used in API calls."""
+    return (datetime.now() - timedelta(minutes=n)).timestamp() * 1000
+
+
 class DataCache:
     """Caches quantitative data from an API into the database after checking the latest timestamp.
 
-    Skips API call if the latest timestamp is more than 5 minutes old."""
+    Skips API call if the latest timestamp is more than DEF_MIN minutes old."""
 
-    def __init__(self, table_name : str):
+    def __init__(self, table_name: str, min_interval: int):
         self._table = metadata.tables.get(table_name)
 
         if self._table is None:
@@ -25,49 +30,52 @@ class DataCache:
             raise ValueError(f"Column 'timestamp_id' does not exist in table {table_name}.")
 
         self._table_name = table_name
-        self._last_call_ts = None
+        self._min_interval = min_interval
+        self._last_called_ts: int | None = None
 
     def __call__(self, f):
-        """Decorator that fetches, processes, and stores new data before returning it."""
         def wrapper(*args, **kwargs):
-            sig = inspect.signature(f)
-            bound_args = sig.bind_partial(*args, **kwargs)
-            bound_args.apply_defaults()
-
-            coin_id = bound_args.arguments.get('coin_id')
-            if coin_id is None:
-                raise ValueError("'coin_id' must be provided.")
-            currency_symbol = bound_args.arguments.get('currency_symbol')
+            coin_id, currency_symbol = self._default_or_custom_coin_and_curr(f, *args, **kwargs)
 
             with DatabaseManager().get_connection() as con:
                 local_data = self.get_local(con, coin_id, currency_symbol)
 
                 if not local_data.empty:
-                    self._last_call_ts = self._last_call_ts or local_data.timestamp.max()
+                    self._last_called_ts = self._last_called_ts or local_data.timestamp.max()
 
-                    if self._last_call_ts > (datetime.now() - timedelta(minutes=5)).timestamp():
+                    if self._last_called_ts > _n_min_ago_timestamp(n=self._min_interval):
                         return local_data
 
-                    kwargs['starting_timestamp'] = self._last_call_ts
+                    kwargs['starting_timestamp'] = self._last_called_ts
 
                 new_data = f(*args, **kwargs)
                 if new_data.empty:
                     return local_data
 
-                try:
-                    self.to_ts_table(con, new_data)
-                    self.to_time_series_table(con, new_data)
-                    con.commit()
-
-                except SQLAlchemyError as e:
-                    con.rollback()
-                    raise SQLAlchemyError(f"Failed to insert new data into {self._table_name}: {e}.")
+                self.to_db(con, new_data)
 
                 return pd.concat([local_data, new_data])
 
         return wrapper
 
-    def get_local(self, connection, coin_id: str, currency_symbol: str):
+    @staticmethod
+    def _default_or_custom_coin_and_curr(f, *args, **kwargs) -> tuple[str, str]:
+        """Inspects the decorated function signature to get actual coin_id and currency_symbol."""
+        sig = inspect.signature(f)
+        bound_args = sig.bind_partial(*args, **kwargs)
+        bound_args.apply_defaults()
+
+        coin_id = bound_args.arguments.get('coin_id')
+        if coin_id is None:
+            raise ValueError("'coin_id' must be provided.")
+
+        currency_symbol = bound_args.arguments.get('currency_symbol')
+        if currency_symbol is None:
+            raise ValueError("'currency_symbol' must be provided.")
+
+        return coin_id, currency_symbol
+
+    def get_local(self, connection, coin_id: str, currency_symbol: str) -> pd.DataFrame:
         joined = timestamps.join(self._table, timestamps.c.id == self._table.c.timestamp_id)
         query = (select(timestamps.c.timestamp, *[col for col in self._table.c if col.name != 'timestamp_id'])
                  .select_from(joined)
@@ -77,9 +85,9 @@ class DataCache:
         result = connection.execute(query)
 
         col_names = result.keys()
-        columns =  zip(*result.fetchall())
+        columns = zip(*result.fetchall())
 
-        loc  = {col_name: column for col_name, column in zip(col_names, columns)}
+        loc = {col_name: column for col_name, column in zip(col_names, columns)}
 
         return make_time_series_frame(loc, coin_id, currency_symbol)
 
@@ -111,3 +119,13 @@ class DataCache:
 
         connection.execute(stmt)
         connection.execute(staging.delete())
+
+    def to_db(self, connection, new_data: pd.DataFrame):
+        try:
+            self.to_ts_table(connection, new_data)
+            self.to_time_series_table(connection, new_data)
+            connection.commit()
+
+        except SQLAlchemyError as e:
+            connection.rollback()
+            raise RuntimeError(f"Failed to insert new data into {self._table_name}.") from e
