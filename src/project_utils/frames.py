83de47fc4e80bade17cf1e66d9fabe181a673b54
Mariusz Huflejt <huflejtm@gmail.com>
2025-05-22 19:15:50 +0200

    Add get_frame_attr() in project_utils/frames

--- src/project_utils/frames.py
+++ src/project_utils/frames.py
@@ -6,3 +6,6 @@ def make_time_series_frame(data: any, coin_id: str, currency_symbol: str) -> pd.
     df.attrs['coin_id'] = coin_id
     df.attrs['currency_symbol'] = currency_symbol
     return df
+
+def get_frame_attr(time_series_frame: pd.DataFrame) -> tuple[str,str]:
+    return time_series_frame.attrs['coin_id'], time_series_frame.attrs['currency_symbol']
